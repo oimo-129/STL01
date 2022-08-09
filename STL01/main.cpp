1fb04cd820320bd363143419047966a745c65d75
zdc <2450289239@qq.com>
2022-08-09 19:48:19 +0800

    添加项目文件。

---
+++ STL01/main.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include"test.h"
+using namespace std;
+int main() {
+	cout << "STL" << endl;
+	//test();
+	//STL容器的初步使用
+	test02();
+	return 0;
+};
---
+++ STL01/test.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<vector>
+#include <algorithm>
+using namespace std;
+void test() {
+	int arr[] = { 1,2,3,4,5 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	int* p = arr;
+	for (int i = 0; i < len; i++) {
+		cout <<  *(p++)<< endl;
+	} 
+}
+void myPrint(int val) {
+	cout << val<<endl;
+}
+//vector STL容器的使用
+void test02() {
+	vector<int>v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.push_back(40);
+	vector<int>::iterator itBegin = v.begin();
+	vector<int>::iterator itEnd = v.end();
+	/*while (itBegin!=itEnd)
+	{
+		cout << *itBegin << endl;
+		itBegin++;
+	}*/
+	//第二种遍历方式
+	/*for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+		cout << *it << endl;
+	}*/
+	//第三种方式
+	for_each(v.begin(), v.end(),myPrint);
+}					  
